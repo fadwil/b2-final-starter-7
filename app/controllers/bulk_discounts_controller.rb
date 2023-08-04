b02f9b5e861fb330a60ad8dd61d1125d72b338d3
Wil Fady <wrfady@gmail.com>
2023-08-04 14:11:37 -0400

    wip: story 2

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -10,6 +10,25 @@ class BulkDiscountsController < ApplicationController
 
   def new
     @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.new
+  end
+  
+  def create
+    @merchant = find_merchant
+    @bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+    if @bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else
+      render :new
+    end
   end
 
+  private
+  def find_merchant
+    Merchant.find(params[:merchant_id])
+  end
+  
+  def bulk_discount_params
+    params.permit(:percentage_discount, :quantity_threshold)
+  end
 end
