032d56c01d2630dd2722fbb87e7040ace35f4830
Wil Fady <wrfady@gmail.com>
2023-08-05 16:27:08 -0400

    feat: edit bulk discount

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -25,6 +25,23 @@ class BulkDiscountsController < ApplicationController
     end
   end
 
+  def edit
+    @merchant = find_merchant
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    @merchant = find_merchant
+    @bulk_discount = BulkDiscount.find(params[:id])
+    if @bulk_discount.update!(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(@merchant, @bulk_discount)
+      flash.notice = "Discount successfully updated!"
+    else
+      redirect_to edit_merchant_bulk_discount_path(@merchant, @bulk_discount)
+      flash.notice = "Field can't be blank"
+    end
+  end
+
   def destroy
     @merchant = find_merchant
     @bulk_discount = @merchant.bulk_discounts.find(params[:id])
